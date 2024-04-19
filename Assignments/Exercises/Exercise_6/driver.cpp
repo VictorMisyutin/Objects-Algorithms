2b639b8949e9749bdc251e20599fc6e26b7fdd4a
Victor Misyutin <misyutinvictor@gmail.com>
2024-04-19 05:46:23 -0500

    Code pretty much completed for Exercise 6

--- Assignments/Exercises/Exercise_6/driver.cpp
+++ Assignments/Exercises/Exercise_6/driver.cpp
@@ -3,9 +3,51 @@
 #include <string>
 #include <unordered_map>
 
-#include "priorityQueue.cpp"
+#include "MinHeap.cpp"
 
 using namespace std;
+
+void BuildHuffmanTable(Node* root, unordered_map<string, string>& ht, ofstream& file, string bitPath) {
+    if(root->GetAsciiCode() != -1){
+        file << root->GetAsciiCode() << " '" << root->GetCharacter() << "' {" << bitPath << "}" << endl;
+        ht[root->GetCharacter()] = bitPath;
+    }
+    if(root->GetLeftChild() == nullptr && root->GetRightChild() == nullptr){
+        // leaf node
+        return;
+    }
+    else if(root->GetLeftChild() == nullptr){
+        // only has right child
+        BuildHuffmanTable(root->GetRightChild(), ht, file, bitPath + "1");
+    }
+    else if(root->GetRightChild() == nullptr){
+        // only has left child
+        BuildHuffmanTable(root->GetLeftChild(), ht, file, bitPath + "0");
+    }
+    else{
+        // has both children
+        BuildHuffmanTable(root->GetLeftChild(), ht, file, bitPath + "0");
+        BuildHuffmanTable(root->GetRightChild(), ht, file, bitPath + "1");
+    }
+}
+
+Node* HuffmanTree(PriorityQueue& pq, unordered_map<string, string>& ht, ofstream& file) {
+    // Node* lastLeft;
+    // Node* lastRight;
+    Node* lastNode;
+    int pass = 0;
+    while (pq.Size() > 1) {
+        Node* leftNode = pq.pop();
+        Node* rightNode = pq.pop();
+        // int freqSum = leftChar.GetFrequency() + rightChar.GetFrequency();
+        int freqSum = leftNode->GetFrequency() + rightNode->GetFrequency();
+        Node* internalNode = new Node(-1, "", freqSum, leftNode, rightNode);
+        pq.enqueue(internalNode);
+        lastNode = internalNode;
+    }
+    // return new Node(pq.peek().GetAsciiCode(), pq.peek().GetCharacter(), pq.peek().GetFrequency(), pq.peek(), lastRight);
+    BuildHuffmanTable(lastNode, ht, file, "");
+}
 
 int main(){
     ifstream inFile("5_in.txt");
@@ -15,8 +57,8 @@ int main(){
     string character = "";
     unordered_map<string, int> characters_map = {};
 
-    characters_map["\\n"] = 0;
     characters_map["\\r"] = 0;
+    characters_map["\\n"] = 0;
 
     while(getline(inFile, line)){
         for(int i = 0; i < line.length(); i++){
@@ -27,12 +69,17 @@ int main(){
                 characters_map[character] += 1;
             }
         }
-        characters_map["\\n"] += 1;
         characters_map["\\r"] += 1;
+        characters_map["\\n"] += 1;
     }
-
+    // back to beginning
+    inFile.clear();
+    inFile.seekg(0);
+    
     string output_character = "";
     int asciiCode = 0;
+    PriorityQueue pq;
+    unordered_map<string, string> hoffmanValues = {};
     debugFile << "countCharacters:\n" << endl;
     for(auto pair: characters_map){
         if(pair.first == "\\n") {
@@ -48,8 +95,28 @@ int main(){
             asciiCode = int(pair.first[0]); 
         }
         debugFile << to_string(asciiCode) << " '" << output_character << "' {" << to_string(pair.second) << "}" << endl;
+        Node* node = new Node(asciiCode, output_character,pair.second);
+        pq.enqueue(node);
     }
 
+    debugFile << "\nprintQueue:\n" << endl;
+    pq.Print(debugFile);
 
+
+    debugFile << "\nmakeBitData:\n" << endl;
+    HuffmanTree(pq, hoffmanValues, debugFile);
+
+    ofstream encodedOut("5_encoded.txt");
+    string encodedString = "";
+    string search = "";
+    while(getline(inFile, line)){
+        for(int i = 0; i < line.length(); i++){
+            character = line[i];
+            encodedString = encodedString + hoffmanValues[character];
+        }
+        encodedString = encodedString + hoffmanValues["\\r"];
+        encodedString = encodedString + hoffmanValues["\\n"];
+    }
+    encodedOut << encodedString;
     return 0;
 }
--- Assignments/Exercises/Exercise_6/priorityQueue.cpp
+++ Assignments/Exercises/Exercise_6/priorityQueue.cpp
@@ -1,29 +1,59 @@
 #include <vector>
 #include <algorithm>
-#include "character.cpp"
-
+#include <iostream>
+#include <fstream>
+#include "Node.cpp"
 using namespace std;
 
 class PriorityQueue{
 private:
-    vector<Character> queue;
+    vector<Node*> queue;
 public:
-    PriorityQueue();
-    void Insert(Character c);
-    Character peek();
-    Character pop();
-    void Print();
-    bool IsEmpty();
-    int Size();
-    bool Contains(string c);
+    PriorityQueue(); // 
+    void enqueue(Node* n); //
+    Node* peek(); //
+    Node* pop(); // 
+    void Print(ofstream& file); //
+    bool IsEmpty(); //
+    int Size(); //
+    bool Contains(Node* n); //
 };
-
-void PriorityQueue::Insert(Character c){
-    // append item to queue
-    queue.push_back(c);
-    // sort in reverse order based on frequency
-    // queue.sort()
-    sort(queue.begin(), queue.end(), [](Character &a, Character &b) {
-        return a.GetFrequency() > b.GetFrequency();
+PriorityQueue::PriorityQueue(){
+    queue = {};
+}
+void PriorityQueue::enqueue(Node* n){
+    queue.push_back(n);
+    sort(queue.begin(), queue.end(), [](Node* a, Node* b) {
+        if(a->GetFrequency() == b->GetFrequency()){
+          return a->GetAsciiCode() < b->GetAsciiCode();
+        }
+        return a->GetFrequency() < b->GetFrequency();
     });
+}
+Node* PriorityQueue::peek(){
+    return queue[0];
+}
+Node* PriorityQueue::pop(){
+    Node* result = queue[0];
+    queue.erase(queue.begin());
+    return result;
+}
+void PriorityQueue::Print(ofstream& file){
+    for(Node* n : queue){
+        file << n->GetAsciiCode() << " '" << n->GetCharacter() << "' {" << n->GetFrequency() << "}" << endl;
+    }
+}
+bool PriorityQueue::IsEmpty(){
+    return queue.size() == 0;
+}
+int PriorityQueue::Size(){
+    return queue.size();
+}
+bool PriorityQueue::Contains(Node* n){
+    for(Node* node : queue){
+        if(node == n){
+            return true;
+        }
+    }
+    return false;
 }
