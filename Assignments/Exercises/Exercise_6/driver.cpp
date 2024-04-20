4553e877b418632c492fee6d68e7cbbe581107fc
Victor Misyutin <misyutinvictor@gmail.com>
2024-04-19 23:35:17 -0600

    working on exercise 6

--- Assignments/Exercises/Exercise_6/driver.cpp
+++ Assignments/Exercises/Exercise_6/driver.cpp
@@ -31,7 +31,7 @@ void BuildHuffmanTable(Node* root, unordered_map<string, string>& ht, ofstream& 
     }
 }
 
-Node* HuffmanTree(PriorityQueue& pq, unordered_map<string, string>& ht, ofstream& file) {
+void HuffmanTree(PriorityQueue& pq, unordered_map<string, string>& ht, ofstream& file) {
     // Node* lastLeft;
     // Node* lastRight;
     Node* lastNode;
@@ -50,73 +50,77 @@ Node* HuffmanTree(PriorityQueue& pq, unordered_map<string, string>& ht, ofstream
 }
 
 int main(){
-    ifstream inFile("5_in.txt");
-    ofstream debugFile("5_debug.txt");
+    for(int i = 5 ; i < 15 ;i+=9 ){
+        ifstream inFile(to_string(i) + "_in.txt");
+        ofstream debugFile( to_string(i) + "_debug.txt");
 
-    string line = "";
-    string character = "";
-    unordered_map<string, int> characters_map = {};
+        string line = "";
+        string character = "";
+        unordered_map<string, int> characters_map = {};
 
-    characters_map["\\r"] = 0;
-    characters_map["\\n"] = 0;
+        characters_map["\\r"] = 0;
+        characters_map["\\n"] = 0;
 
-    while(getline(inFile, line)){
-        for(int i = 0; i < line.length(); i++){
-            character = line[i];
-            if(characters_map.find(character) == characters_map.end())
-                characters_map[character] =  1;
-            else{
-                characters_map[character] += 1;
+        while(getline(inFile, line)){
+            cout << "new line" << endl;
+            for(int i = 0; i < line.length(); i++){
+                character = line[i];
+                if(characters_map.find(character) == characters_map.end())
+                    characters_map[character] =  1;
+                else{
+                    characters_map[character] += 1;
+                }
             }
+            characters_map["\\r"] += 1;
+            characters_map["\\n"] += 1;
         }
-        characters_map["\\r"] += 1;
-        characters_map["\\n"] += 1;
+        cout << endl;
+        // back to beginning
+        inFile.clear();
+        inFile.seekg(0);
+        
+        string output_character = "";
+        int asciiCode = 0;
+        PriorityQueue pq;
+        unordered_map<string, string> hoffmanValues = {};
+        debugFile << "countCharacters:\n" << endl;
+        for(auto pair: characters_map){
+            if(pair.first == "\\n") {
+                output_character = "\\n";
+                asciiCode = 10;        
+            }
+            else if(pair.first == "\\r") {
+                output_character = "\\r";
+                asciiCode = 13;
+            }
+            else{
+                output_character = pair.first;
+                asciiCode = int(pair.first[0]); 
+            }
+            debugFile << to_string(asciiCode) << " '" << output_character << "' {" << to_string(pair.second) << "}" << endl;
+            Node* node = new Node(asciiCode, output_character,pair.second);
+            pq.enqueue(node);
+        }
+
+        debugFile << "\nprintQueue:\n" << endl;
+        pq.Print(debugFile);
+
+
+        debugFile << "\nmakeBitData:\n" << endl;
+        HuffmanTree(pq, hoffmanValues, debugFile);
+
+        ofstream encodedOut(to_string(i) + "_encoded.txt");
+        string encodedString = "";
+        string search = "";
+        while(getline(inFile, line)){
+            for(int i = 0; i < line.length(); i++){
+                character = line[i];
+                encodedString = encodedString + hoffmanValues[character];
+            }
+            encodedString = encodedString + hoffmanValues["\\r"];
+            encodedString = encodedString + hoffmanValues["\\n"];
+        }
+        encodedOut << encodedString;
     }
-    // back to beginning
-    inFile.clear();
-    inFile.seekg(0);
-    
-    string output_character = "";
-    int asciiCode = 0;
-    PriorityQueue pq;
-    unordered_map<string, string> hoffmanValues = {};
-    debugFile << "countCharacters:\n" << endl;
-    for(auto pair: characters_map){
-        if(pair.first == "\\n") {
-            output_character = "\\n";
-            asciiCode = 10;        
-        }
-        else if(pair.first == "\\r") {
-            output_character = "\\r";
-            asciiCode = 13;
-        }
-        else{
-            output_character = pair.first;
-            asciiCode = int(pair.first[0]); 
-        }
-        debugFile << to_string(asciiCode) << " '" << output_character << "' {" << to_string(pair.second) << "}" << endl;
-        Node* node = new Node(asciiCode, output_character,pair.second);
-        pq.enqueue(node);
-    }
-
-    debugFile << "\nprintQueue:\n" << endl;
-    pq.Print(debugFile);
-
-
-    debugFile << "\nmakeBitData:\n" << endl;
-    HuffmanTree(pq, hoffmanValues, debugFile);
-
-    ofstream encodedOut("5_encoded.txt");
-    string encodedString = "";
-    string search = "";
-    while(getline(inFile, line)){
-        for(int i = 0; i < line.length(); i++){
-            character = line[i];
-            encodedString = encodedString + hoffmanValues[character];
-        }
-        encodedString = encodedString + hoffmanValues["\\r"];
-        encodedString = encodedString + hoffmanValues["\\n"];
-    }
-    encodedOut << encodedString;
     return 0;
 }
