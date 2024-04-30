8abc9331718353c6a56968ebb2b688d77ae22311
Victor Misyutin <misyutinvictor@gmail.com>
2024-04-30 00:25:48 -0500

    Working on lab 7

--- Assignments/Labs/lab7/driver.cpp
+++ Assignments/Labs/lab7/driver.cpp
@@ -3,8 +3,14 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <iomanip> // for setw and setfill
+#include <iomanip>
+#include <locale>
+#include <codecvt>
+#include <io.h>
+#include <fcntl.h>
 #include <boost/multiprecision/cpp_int.hpp>
+
+#include "FileComparator.cpp"
 
 using namespace std;
 using namespace boost::multiprecision;
@@ -93,6 +99,62 @@ int calculate_block_length(const cpp_int& N) {
     return block_length;
 }
 
+string decryptRSA(const string& ciphertext, const cpp_int& N, const cpp_int& D, ofstream& df, int& length) {
+    // Convert ciphertext from hexadecimal to decimal
+    istringstream iss(ciphertext);
+    cpp_int c_dec;
+    iss >> hex >> c_dec;
+
+    // Perform modular exponentiation to decrypt the ciphertext
+    cpp_int plaintext = powm(c_dec, D, N);
+
+    // Convert plaintext from cpp_int to string
+    ostringstream oss;
+    oss << hex << plaintext;
+    string plaintext_hex = oss.str();
+    df << "RSAAlgorithm::decrypt 0x" << hex << uppercase << ciphertext << "-> 0x" << plaintext_hex << "\n" << endl;
+    df << "BlockWriter::writeData - Writing " << plaintext_hex << " as "  << length << " bytes\n" << endl;
+
+    // Convert hexadecimal plaintext to ASCII
+    string plaintext_ascii;
+    int count = 0;
+    for (size_t i = 0; i < plaintext_hex.length(); i += 2) {
+        string byte = plaintext_hex.substr(i, 2);
+        char chr = static_cast<char>(stoi(byte, nullptr, 16));
+        if (chr == '\r') {
+            df << "[" << count << "] '\\r'";
+        }
+        else if (chr == '\n') {
+            df << "[" << count << "] '\\n'";
+            plaintext_ascii.push_back(chr);
+        }
+        else {
+            df << "[" << count << "] '" << chr << "'";
+            plaintext_ascii.push_back(chr);
+        }
+    }
+    df << "\n" << endl;
+
+    return plaintext_ascii;
+}
+
+bool compareFiles(const string& p1, const string& p2) {
+    ifstream f1(p1, ifstream::binary | ifstream::ate);
+    ifstream f2(p2, ifstream::binary | ifstream::ate);
+
+    if (f1.fail() || f2.fail()) {
+        cout << "first fail";
+        return false;
+    }
+
+    f1.seekg(0, ifstream::beg);
+    f2.seekg(0, ifstream::beg);
+    bool result = equal(istreambuf_iterator<char>(f1.rdbuf()),
+        istreambuf_iterator<char>(),
+        istreambuf_iterator<char>(f2.rdbuf()));
+    return result;
+}
+
 int main() {
     for (int a = 0; a < 4; a++) {
         cpp_int P;
@@ -115,47 +177,101 @@ int main() {
         // Read data from file
         ifstream inFile(to_string(a) + "_in.dat");
         ostringstream buffer;
-        if (inFile.peek() == '\xEF') {
-            inFile.get();
-            if (inFile.get() != '\xBB' || inFile.get() != '\xBF') {
-                // Invalid BOM, handle error or reset the stream
-                // inFile.seekg(0); // Reset the stream position if needed
-            }
-        }
         buffer << inFile.rdbuf();
         string plaintext = buffer.str();
         inFile.close();
-        cout << plaintext << "===================" << endl;
-        debugFile << "*** Encrypting " << to_string(a) << "_in.dat, block length: " << block_length << " -> " << to_string(a) << "_encrypted.txt ***" << endl;
+        debugFile << "\n*** Encrypting " << to_string(a) << "_in.dat, block length: " << block_length << " -> " << to_string(a) << "_encrypted.txt ***\n" << endl;
         ofstream encryptedFile(to_string(a) + "_encrypted.txt");
         encryptedFile.flush();
-        for (size_t i = 0; i < plaintext.size(); i += block_length) {
-            string block = plaintext.substr(i, block_length);
+        vector<string> blocks;
+        size_t pos = 0;
+        for (int i = 0; i < plaintext.size(); i++) {
+            if (plaintext[i] == '\n') {
+                plaintext.erase(i, 1);
+                plaintext.insert(i, "\r\n");
+                i++;
+            }
+        }
+        while (pos < plaintext.size()) {
+            string block = plaintext.substr(pos, block_length);
+            pos += block_length ;
+            blocks.push_back(block);
+        }
+
+        // Encrypt each block
+        int blockCount = 0;
+        for (const string& block : blocks) {
             cpp_int plaintextNumber = 0;
-            debugFile << "BlockReader::readData - Requested count "<< block_length << " bytes, got" << endl;
+            // Convert characters to numbers
+            debugFile << "--- RSACodec::encryptStream block #" << dec << blockCount << ", max length " << dec << block_length << " --- \n" << endl;
+            debugFile << "BlockReader::readData - Requested count " << dec << block_length << " bytes, got" << endl;
             int count = 0;
             for (char c : block) {
-                if (c == '\n') {
+                if (c == '\r') {
                     debugFile << "[" << count << "] '\\r' ";
                     plaintextNumber = plaintextNumber * 256 + static_cast<unsigned char>('\r');
-                    count++;
+                }
+                else if (c == '\n') {
                     debugFile << "[" << count << "] '\\n' ";
                     plaintextNumber = plaintextNumber * 256 + static_cast<unsigned char>('\n');
                 }
-                else{
+                else {
                     debugFile << "[" << count << "] '" << c << "' ";
                     plaintextNumber = plaintextNumber * 256 + static_cast<unsigned char>(c);
                 }
                 count++;
             }
-            debugFile << endl;
+            // Encrypt the block and write to encrypted file
             cpp_int ciphertextNumber = rsa_encrypt(plaintextNumber, E, N);
-
-            encryptedFile << hex << block_length << " " << ciphertextNumber << endl;
+            if (count == block_length) {
+                debugFile << endl << "BlockReader::readData - Read 0x" << hex << uppercase << plaintextNumber << " as " << dec << block_length << " bytes\n" << endl;
+                encryptedFile << hex << uppercase << block_length << " " << ciphertextNumber << endl;
+            }
+            else {
+                debugFile << endl << "BlockReader::readData - Read 0x" << hex << uppercase << plaintextNumber << " as " << dec << count << " bytes\n" << endl;
+                encryptedFile << hex << uppercase << count << " " << ciphertextNumber << endl;
+            }
+            debugFile << "RSAAlgorithm::decrypt 0x" << hex << uppercase << plaintextNumber << " -> 0x" << hex << uppercase << ciphertextNumber << "\n" << endl;
+            blockCount++;
         }
         encryptedFile.close();
-        debugFile << "*** Decrypting " << to_string(a) << "_in.dat, block length: " << block_length << " -> " << to_string(a) << "_decrypted.txt ***" << endl;
+        // size is the sum of bytes, count number of lines * block_length + count_of_last
+        debugFile << "*** Decrypting " << to_string(a) << "_in.dat, block length: " << block_length << " -> " << to_string(a) << "_decrypted.txt ***\n " << endl;
 
+        ifstream ciphertextFile(to_string(a) + "_encrypted.txt");
+        ofstream decryptedFile(to_string(a) + "_decrypted.dat");
+
+        string ciphertext;
+        int lineCount = 1;
+        int length = 0;
+        while (getline(ciphertextFile, ciphertext)) {
+            unsigned int x;
+            stringstream ss;
+            ss << dec << ciphertext.substr(0, ciphertext.find(' '));
+            ss >> length;
+            ciphertext = ciphertext.substr(ciphertext.find(' '), ciphertext.size());
+            debugFile << "--- RSACodec::decryptStream block #" << lineCount << ", length " << length << " --- " << endl;
+            string plaintext = decryptRSA(ciphertext, N, D, debugFile, length);
+            decryptedFile << plaintext;
+            lineCount++;
+        }
+        ciphertextFile.close();
+        decryptedFile.close();
+        //try {
+        //    FileComparator::compareFiles(to_string(a) + "_in.dat", to_string(a) + "_decrypted.dat");
+        //    cout << to_string(a) << ": Files are identical." << endl;
+        //}
+        //catch (const exception& e) {
+        //    cerr << "Error: " << e.what() << endl;
+        //}
+        //cout << endl;
+        if (compareFiles(to_string(a) + "_in.dat", to_string(a) + "_decrypted.dat")) {
+            cout << to_string(a) << " the same " << endl;
+        }
+        else {
+            cout << to_string(a) << " different" << endl;
+        }
+        
     }
     return 0;
 }
