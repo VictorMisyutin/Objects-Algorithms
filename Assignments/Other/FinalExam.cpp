61fde1c2d821d140afe269c307af57d889a993aa
Victor Misyutin <misyutinvictor@gmail.com>
2024-05-08 19:10:06 -0600

    post final exam

---
+++ Assignments/Other/FinalExam.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int result = 85%143;
+    cout << result << endl;
+}
---
+++ Assignments/Other/test.py
@@ -0,0 +1,7 @@
+i = 1
+result = ((220*i)+1)/223
+while result.is_integer() != True :
+    i = i+1
+    result = ((220*i)+1)/223
+print(result)
+print(i)
